c066d82d7a67dd9b99af99e3db67059c8cd15a47
Karl Voelker <ktvoelker@gmail.com>
2012-06-24 14:38:06 -0700

    Added status exceptions and Vorple type

--- Comm.hs
+++ Comm.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Comm where
 
 import Control.Monad.Error
@@ -14,10 +16,13 @@ import Data.List
 import Data.Maybe
 import qualified Data.Text.Lazy as T
 import GHC.Exts (fromString)
-import Network.HTTP.Types (Status(), unauthorized401, StdMethod())
+import Network.HTTP.Types (Status(), status500, status401, StdMethod())
 import Network.Wai (requestHeaders)
 import System.Random
 import Web.Scotty
+
+instance Error Status where
+  noMsg = status500
 
 class (Monad m) => MonadAction m where
   liftActionM :: ActionM a -> m a
@@ -31,6 +36,18 @@ instance (MonadAction m) => MonadAction (ReaderT e m) where
 instance (Error e, MonadAction m) => MonadAction (ErrorT e m) where
   liftActionM = lift . liftActionM
 
+newtype Vorple e a = Vorple { getv :: ErrorT Status (ReaderT e ActionM) a }
+  deriving (Monad, MonadAction)
+
+instance MonadError Status (Vorple e) where
+  throwError = Vorple . throwError
+  catchError m f = Vorple $ getv m `catchError` (getv . f)
+
+instance MonadReader e (Vorple e) where
+  ask = Vorple ask
+  local f = Vorple . local f . getv
+  reader = Vorple . reader
+
 class EnvAdd a e f | a e -> f where
   envAdd :: a -> e -> f
 
@@ -40,9 +57,8 @@ newtype Secret = Secret { secretValue :: Int } deriving (Eq, Ord, Read, Show)
 
 data Cookie = Cookie User Secret deriving (Eq, Ord, Read, Show)
 
--- TODO catch status exceptions
-run :: (ToJSON a, MonadAction m, MonadReader e m) => m a -> m ()
-run = (>>= liftActionM . json)
+run :: (ToJSON a, MonadAction m, MonadError Status m) => m a -> m ()
+run = (`catchError` (liftActionM . status)) . (>>= liftActionM . json)
 
 cookieMarker :: String
 cookieMarker = "session="
@@ -78,13 +94,13 @@ setCookie =
 makeSecret :: ActionM Secret
 makeSecret = liftIO (getStdRandom random) >>= return . Secret
 
-withJson
-  :: forall a b e f m. (FromJSON a, EnvAdd a e f, MonadAction m)
-  => ReaderT f m b
-  -> ReaderT e m b
+mapEnv :: (e -> f) -> Vorple f a -> Vorple e a
+mapEnv f = Vorple . mapErrorT (withReaderT f) . getv
+
+withJson :: forall a b e f m. (FromJSON a, EnvAdd a e f) => Vorple f b -> Vorple e b
 withJson m = do
   j <- liftActionM jsonData
-  withReaderT (envAdd (j :: a)) m
+  mapEnv (envAdd (j :: a)) m
 
 doesAuth :: (MonadAction m) => m (User, b) -> m b
 doesAuth m = do
@@ -92,17 +108,14 @@ doesAuth m = do
   liftActionM $ makeSecret >>= setCookie . Cookie u
   return x
 
-mustAuth
-  :: (EnvAdd User e f, MonadAction m)
-  => ReaderT f m b
-  -> ReaderT e m b
+mustAuth :: (EnvAdd User e f) => Vorple f b -> Vorple e b
 mustAuth m = do
   c <- liftActionM getCookie
   case c of
-    Nothing -> undefined --liftIO $ throw unauthorized401
+    Nothing -> throwError status401
     Just (Cookie u s) -> do
       a <- liftActionM $ param "secret"
       if Secret a == s
-      then withReaderT (envAdd u) m
-      else undefined --liftIO $ throw unauthorized401
+      then mapEnv (envAdd u) m
+      else throwError status401
 
