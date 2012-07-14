b82129d7a297843deea817732e2d11d3fd8d8638
Karl Voelker <ktvoelker@gmail.com>
2012-07-14 09:22:31 -0700

    More work on showing SELECTs

--- src/Database/VorPg/Sql.hs
+++ src/Database/VorPg/Sql.hs
@@ -31,14 +31,15 @@ markFirst :: [a] -> [(Bool, a)]
 markFirst [] = []
 markFirst (x : xs) = (True, x) : map (False, ) xs
 
+sepByM :: (Monad m) => m () -> [m ()] -> m ()
+sepByM sep xs = flip mapM_ (markFirst xs) $ \(first, x) -> when (not first) sep >> x
+
 instance ShowSql Stmt where
   showsSql (Insert table cols rel) = s $ do
     lit "INSERT INTO "
     add table
     lit "("
-    flip mapM_ (markFirst cols) $ \(first, col) -> do
-      when (not first) $ lit ", "
-      add col
+    sepByM (lit ", ") $ map add cols
     lit ")"
     add rel
     lit ";"
@@ -46,8 +47,7 @@ instance ShowSql Stmt where
     lit "UPDATE "
     add table
     lit "SET "
-    flip mapM_ (markFirst changes) $ \(first, (col, val)) -> do
-      when (not first) $ lit ", "
+    sepByM (lit ", ") $ flip map changes $ \(col, val) -> do
       add col
       lit " = "
       add val
@@ -60,14 +60,125 @@ instance ShowSql Stmt where
     lit " WHERE "
     add restrict
     lit ";"
-  -- TODO SELECT
+  showsSql (Select rel) = s $ do
+    add rel
+    lit ";"
 
 instance ShowSql Name where
   showsSql = showsSql . fromString . getName
 
--- TODO
+whenJust :: (Monad m) => Maybe a -> (a -> m ()) -> m ()
+whenJust = flip $ maybe (return ())
+
+showsSqlTable :: SqlTable -> Builder -> Builder
+showsSqlTable SqlTable{..} = s $ do
+  add stName
+  whenJust stAlias $ \a -> do
+    lit " "
+    add a
+showsSqlTable SqlSubQuery{..} = s $ do
+  lit "("
+  add stQuery
+  lit ") "
+  add stQAlias
+
+data Join' = InnerJoin | LeftOuterJoin | RightOuterJoin | FullOuterJoin
+  deriving (Show)
+
+instance ShowSql Join' where
+  showsSql InnerJoin      = s $ lit "INNER JOIN"
+  showsSql LeftOuterJoin  = s $ lit "LEFT OUTER JOIN"
+  showsSql RightOuterJoin = s $ lit "RIGHT OUTER JOIN"
+  showsSql FullOuterJoin  = s $ lit "FULL OUTER JOIN"
+
+showsSqlRel :: SqlRel -> Builder -> Builder
+showsSqlRel SqlRel{..} = s $ do
+    lit "SELECT "
+    case srProject of
+      Nothing -> lit "*"
+      Just ps -> sepByM (lit ", ") $ flip map ps $ \(name, val) -> do
+        add val
+        lit " "
+        add name
+    whenJust srFirstTable $ \ft -> do
+      lit " FROM "
+      modify $ showsSqlTable ft
+      flip mapM_ srTables $ \(join, table, val) -> do
+        lit " "
+        add join
+        lit " "
+        modify $ showsSqlTable table
+        whenJust val $ \v -> do
+          lit " ON "
+          add v
+    whenJust srRestrict $ \r -> do
+      lit " WHERE "
+      add r
+    whenJust srGroup $ \gs -> do
+      lit " GROUP BY "
+      sepByM (lit ", ") $ map add gs
+    whenJust srGroupRestrict $ \gr -> do
+      lit " HAVING "
+      add gr
+    whenJust srOrderBy $ \os -> do
+      lit " ORDER BY "
+      sepByM (lit ", ") $ flip map os $ \(val, dir) -> add val >> add dir
+
+data SqlTable =
+  SqlTable
+  { stName  :: Name
+  , stAlias :: Maybe Name
+  } |
+  SqlSubQuery
+  { stQuery :: Builder
+  , stQAlias :: Name
+  } deriving (Show)
+
+data SqlRel = SqlRel
+  -- first table
+  { srFirstTable    :: Maybe SqlTable
+  -- subsequent tables
+  , srTables        :: [(Join', SqlTable, Maybe Val)]
+  -- projections
+  , srProject       :: Maybe [(Name, Val)]
+  -- row restrictions
+  , srRestrict      :: Maybe Val
+  -- groups
+  , srGroup         :: Maybe [Val]
+  -- group restrictions
+  , srGroupRestrict :: Maybe Val
+  -- ordering
+  , srOrderBy       :: Maybe [(Val, Dir)]
+  } deriving (Show)
+
+emptySqlRel = SqlRel
+  { srFirstTable    = Nothing
+  , srTables        = []
+  , srProject       = Nothing
+  , srRestrict      = Nothing
+  , srGroup         = Nothing
+  , srGroupRestrict = Nothing
+  , srOrderBy       = Nothing
+  }
+
+makeSqlRel :: Rel -> State SqlRel ()
+makeSqlRel _ = undefined
+
+showsSelect :: Rel -> Builder -> Builder
+showsSelect r = showsSqlRel $ execState (makeSqlRel r) emptySqlRel
+
 instance ShowSql Rel where
-  showsSql _ = ("" `mappend`)
+  showsSql (RUnion q r) = s $ do
+    lit "("
+    modify $ showsSelect q
+    lit ") UNION ("
+    modify $ showsSelect r
+    lit ")"
+  showsSql r = showsSelect r
+
+instance ShowSql Dir where
+  showsSql Asc  = s $ lit "ASC"
+  showsSql Desc = s $ lit "DESC"
 
 -- TODO
 instance ShowSql Val where
