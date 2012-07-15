5ac493dc9a017f90830107b61da073751eafdd9b
Karl Voelker <ktvoelker@gmail.com>
2012-07-14 19:15:11 -0700

    Removed VorPg experiment

--- src/Database/VorPg.hs
+++
@@ -1,7 +0,0 @@
-
-module Database.VorPg
-  ( module Database.VorPg.Lang
-  ) where
-
-import Database.VorPg.Lang
-
--- src/Database/VorPg/Lang.hs
+++
@@ -1,9 +0,0 @@
-
-module Database.VorPg.Lang
-  ( module Database.VorPg.Lang.Stmt
-  , module Database.VorPg.Lang.Val
-  ) where
-
-import Database.VorPg.Lang.Stmt
-import Database.VorPg.Lang.Val
-
