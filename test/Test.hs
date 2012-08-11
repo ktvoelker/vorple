19f053a43dea6947ce7e8e7fe6c5777318dd3eea
Karl Voelker <ktvoelker@gmail.com>
2012-08-10 22:27:50 -0700

    Added a stateful test case

--- test/Test.hs
+++ test/Test.hs
@@ -17,12 +17,59 @@ testEcho1 = sessionTest "testEcho1" appEcho $ do
   r1 <- rj $ Csrf "" b1
   assertStatus 200 r1
   assertNoCookie r1
-  void $ assertJsonBody b1 r1
+  k1 <- assertJsonBody b1 r1
   let b2 = Rec1a 1 0
   r2 <- rj $ Csrf "" b2
   assertStatus 200 r2
   assertNoCookie r2
-  void $ assertJsonBody b2 r2
+  k2 <- assertJsonBody b2 r2
+  assertBool "CSRF keys should not be equal" $ k1 /= k2
 
-main = runTests [testEcho1]
+data Cmd
+  = CmdGet
+  | CmdPut Rec1
+  deriving (Eq, Ord, Read, Show)
+
+$(deriveJSON id ''Cmd)
+
+initState = Rec1a 0 0
+
+appState = vorple defaultOptions () initState $ \r -> case r of
+  CmdGet   -> get
+  CmdPut x -> do
+    prev <- get
+    put x
+    $(debug "put %j") x
+    return prev
+
+testState1 = sessionTest "testState1" appState $ do
+  -- get initial state
+  r1 <- rj $ Csrf "" CmdGet
+  assertStatus 200 r1
+  k1 <- assertJsonBody initState r1
+  -- set b2, returning initial state
+  let b2 = Rec1b 1 2
+  r2 <- rj $ Csrf k1 $ CmdPut b2
+  assertStatus 200 r2
+  k2 <- assertJsonBody initState r2
+  assertBool "CSRF keys should not be equal" $ k1 /= k2
+  c2 <- assertCookie r2
+  -- set b3, returning b2
+  let b3 = Rec1b 3 4
+  r3 <- rjc c2 $ Csrf k2 $ CmdPut b3
+  assertStatus 200 r3
+  k3 <- assertJsonBody b2 r3
+  assertEqual "CSRF keys" k2 k3
+  c3 <- assertCookie r3
+  -- get b3
+  r4 <- rjc c3 $ Csrf k3 CmdGet
+  assertStatus 200 r4
+  k4 <- assertJsonBody b3 r4
+  assertEqual "CSRF keys" k3 k4
+
+main =
+  runTests
+  [ testEcho1
+  , testState1
+  ]
 
--- test/Util.hs
+++ test/Util.hs
@@ -7,19 +7,31 @@ module Util
   , assertStatus
   ) where
 
+import qualified Data.ByteString as SBS
+import qualified Data.Text as ST
+import qualified Data.Text.Encoding as STE
+import qualified Test.HUnit as H
+
 import Control.Monad
 import Data.Aeson
 import Data.Aeson.TH
-import Data.ByteString.Lazy
-import Data.Text.Lazy
 import Data.Text.Lazy.Encoding
-import Test.HUnit
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Test
 import System.Exit
 import System.IO
 import Web.Vorple
+import Web.Vorple.Text
+
+assertEqual :: (Show a, Eq a) => String -> a -> a -> Session ()
+assertEqual p a b = liftIO $ H.assertEqual p a b
+
+assertBool :: String -> Bool -> Session ()
+assertBool p = liftIO . H.assertBool p
+
+assertFailure :: String -> Session ()
+assertFailure = liftIO . H.assertFailure
 
 data Csrf a
   = Csrf { csrfKey :: String, csrfData :: a }
@@ -27,19 +39,26 @@ data Csrf a
 
 $(deriveJSON (Prelude.drop 4) ''Csrf)
 
-rb :: ByteString -> Session SResponse
-rb = srequest . SRequest (defaultRequest { requestMethod = methodPost })
-
-rt :: Text -> Session SResponse
-rt = rb . encodeUtf8
+defaultPost = defaultRequest { requestMethod = methodPost }
 
 rj :: (ToJSON a) => a -> Session SResponse
-rj = rb . encode
+rj = srequest . SRequest defaultPost . encode
+
+rjc :: (ToJSON a) => String -> a -> Session SResponse
+rjc cookie =
+  srequest
+  . SRequest
+    ( defaultPost
+      { requestHeaders
+        = ("Cookie", STE.encodeUtf8 $ ST.pack cookie) : requestHeaders defaultPost
+      }
+    )
+  . encode
 
 -- | Assert that the response body is the given JSON object wrapped in a Csrf
 -- object, and return the CSRF key string.
 assertJsonBody :: (FromJSON a, Eq a, Show a) => a -> SResponse -> Session String
-assertJsonBody expBody x = liftIO $ do
+assertJsonBody expBody x = do
   case decode $ simpleBody x of
     Nothing -> do
       assertFailure "Failed to parse response body"
@@ -51,11 +70,16 @@ assertJsonBody expBody x = liftIO $ do
 assertNoCookie :: SResponse -> Session ()
 assertNoCookie = assertNoHeader "Set-Cookie"
 
-sessionTest :: String -> Application -> Session () -> Test
-sessionTest n a s = TestLabel n $ TestCase $ runSession s a
+assertCookie :: SResponse -> Session String
+assertCookie resp = case filter ((== "Set-Cookie") . fst) $ simpleHeaders resp of
+  []      -> assertFailure "No cookie" >> return ""
+  (c : _) -> return $ ST.unpack $ STE.decodeUtf8 $ snd c
 
-runTests :: [Test] -> IO ()
+sessionTest :: String -> Application -> Session () -> H.Test
+sessionTest n a s = H.TestLabel n $ H.TestCase $ runSession s a
+
+runTests :: [H.Test] -> IO ()
 runTests tests = do
-  c <- runTestTT $ TestList tests
-  when (errors c /= 0 || failures c /= 0) exitFailure
+  c <- H.runTestTT $ H.TestList tests
+  when (H.errors c /= 0 || H.failures c /= 0) exitFailure
 
