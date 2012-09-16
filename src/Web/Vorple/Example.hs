f34c0e7522c84d071c241869ff775246f9636ea5
Karl Voelker <ktvoelker@gmail.com>
2012-09-16 00:28:46 -0700

    Added an example program

--- src/Web/Vorple/Example.hs
+++ src/Web/Vorple/Example.hs
@@ -1,3 +1,5 @@
+-- GENERATED; DO NOT EDIT!
+{- |
 This is an example application built with Vorple. An 'Int' is stored in each session,
 and the client can send two kinds of requests: one to get the current value, and
 another to set the value while returning the existing value.
@@ -7,25 +9,33 @@ Web.Vorple.Example, it was generated automatically from @example/Simple.lhs@ in 
 Vorple source code repository, which the Cabal configuration for Vorple will build as
 an executable called @vorple-example-simple@.
 
-> module Main where
+@
+ module Main where
+@
 
 You'll hear about these imports later.
 
-> import Control.Monad.Identity (Identity())
-> import Network.Wai.Handler.Warp (run)
-> import Web.Vorple
+@
+ import Control.Monad.Identity (Identity())
+ import Network.Wai.Handler.Warp (run)
+ import Web.Vorple
+@
 
 We need types for requests, session state, and responses.
 
-> data Request = Get | Put Int
-> type Session = Int
-> type Response = Int
+@
+ data Request = Get | Put Int
+ type Session = Int
+ type Response = Int
+@
 
 Since the request type is a new one of our own invention, it doesn't yet have a
 'FromJSON' instance. We can derive one automatically by calling this Template Haskell
 function from "Data.Aeson.TH":
 
-> $(deriveJSON id ''Request)
+@
+ $(deriveJSON id ''Request)
+@
 
 Now we need a request handler. It's basically just a function from Request to
 Response, with a monad along the way. Which monad is it? It's the
@@ -43,16 +53,22 @@ instance, which we aren't using here.
 * 'Identity': a monad that doesn't really do anything. Vorple needs an underlying
 monad, but in this case, we don't, so @Identity@ is a good choice.
 
-> handler :: Request -> Vorple () Session Identity Response
-> handler Get = get
-> handler (Put n) = do
->   prev <- get
->   put n
->   return prev
+@
+ handler :: Request -> Vorple () Session Identity Response
+ handler Get = get
+ handler (Put n) = do
+   prev <- get
+   put n
+   return prev
+@
 
 Finally, we can use the vorple function to turn our handler into a standard WAI
 'Application' which we can run with Warp.
 
-> main :: IO ()
-> main = run 4242 $ vorple defaultOptions () 0 handler
+@
+ main :: IO ()
+ main = run 4242 $ vorple defaultOptions () 0 handler
+@
 
+-}
+module Web.Vorple.Example where
